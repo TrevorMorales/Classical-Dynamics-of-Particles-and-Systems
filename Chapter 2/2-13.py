3dc612c3e3fdfba8f7ef0c7f68761f48f8a99f5e
TrevorMorales <trevorjmorales@gmail.com>
2024-09-17 00:04:12 -0400

    2-13 Complete

--- Chapter 2/2-13.py
+++ Chapter 2/2-13.py
@@ -1,27 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import os
 
-ConstantA = 0.5
-ConstantK = 0.3
-InitialVelocity = 400
-Steps = 1000
-TimeSteps = np.linspace(0, Steps-1, Steps)
-Distance = np.zeros((Steps))
-dT = 0.001
+Directory = os.path.dirname(__file__)
 
-Velocity = InitialVelocity
-Distance[0] = 0
+ConstantA = 0.0005
+ConstantK = 0.001
+InitialVelocity = 40
 
-for i in range(1, Steps):
-    Time = TimeSteps[i] * dT
-    Distance[i] = (1 / ( ConstantA * ConstantK * 2)) * -math.asin( ( ConstantA * ConstantA - Velocity * Velocity ) / ( ConstantA * ConstantA + Velocity * Velocity ) )
+Steps = 10000
+dT = 0.01
+Time = np.zeros((Steps))
+Force = np.zeros((Steps))
+Acceleration = np.zeros((Steps))
+Velocity = np.zeros((Steps))
+Position = np.zeros((Steps))
+Mass = 2
+Velocity[0] = InitialVelocity
 
-print(Distance)
+for i in range(1,Steps):
+    Time[i] = i * dT
+    Force[i] = -Mass * ConstantK * (Velocity[i-1] * Velocity[i-1] * Velocity[i-1] + ConstantA * ConstantA * Velocity[i-1])
+    Acceleration[i] = Force[i] / Mass
+    Velocity[i] = Velocity[i-1] + Acceleration[i-1] * dT
+    Position[i] = Position[i-1] + Velocity[i-1] * dT
 
-plt.plot(TimeSteps*dT, Distance)
-plt.axvline(x=math.pi/(2 * ConstantA * ConstantK))
+plt.plot(Velocity, Time)
+plt.ylabel("Velocity (m/s)")
+plt.xlabel("Time (s)")
+plt.savefig(Directory + '/2-13_Velocity_v_Time')
 plt.show()
-
+plt.plot(Time, Position)
+plt.ylabel("Position (m)")
+plt.xlabel("Time (s)")
+plt.savefig(Directory + '/2-13_Position_v_Time')
+plt.show()
 # m, k, a constants
 # RetardingForce = m * k * (v * v * v + a * a * v)
